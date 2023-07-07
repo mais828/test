aff9f500cfa515d2b0c2731f64dd4a3a43c1e76a
方工 <111@163.com>
2023-07-07 13:52:16 +0800

    first commit

---
+++ src/Echo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Friker\Test;
+
+class Echo
+{ 
+    public function xml($data = array())
+    { 
+        header("Content-type:application/json");
+        echo '<?xml version="1.0" encoding="utf-8"?>'
+        echo '<data>';
+        foreach($data as $k => $v){ 
+           echo "<{ $k}>$v</$k>";
+        }
+        echo '</data>';
+        exit;
+    }
+
+    public function json($data = array())
+    { 
+        header("Content-type:application/json");
+        echo json_encode($data);
+        exit;
+    }
+}
